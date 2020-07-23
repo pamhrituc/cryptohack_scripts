09deaab329ed880c9f964148bbec609ff0b75966
pamhrituc <pam997.PH@gmail.com>
2020-07-23 19:16:27 +0300

    Did a fix in fromhex

--- encoding_challenge/fromhex.py
+++ encoding_challenge/fromhex.py
@@ -1,6 +1,6 @@
 import sys
 
 encoded = sys.argv[1]
-flag = str(bytearray.fromhex(encoded))
-result = "{\"decoded\": \"" + flag[12:-2] + "\"}"
+flag = str(bytes.fromhex(encoded))[2:-1]
+result = "{\"decoded\": \"" + flag + "\"}"
 print(result)
